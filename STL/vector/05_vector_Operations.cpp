15d9cf3428cf93fa2a354b0be479e64af11d67f9
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-08-26 14:11:39 +0530

    practice array

---
+++ STL/vector/05_vector_Operations.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter the elements(N)" << endl;
+    cin >> n;
+
+    vector<int> elements;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        elements.push_back(val);
+    }
+
+    cout << "elements before removing" << endl;
+    for (int num : elements) {
+        cout << num << " ";
+    }
+    cout << endl; // Added a line break for cleaner output
+
+    if (!elements.empty()) {
+        elements.pop_back(); 
+    }
+
+    cout << "elements after removing" << endl;
+    for (int nums : elements) {
+        cout << nums << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
