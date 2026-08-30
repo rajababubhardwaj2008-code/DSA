461738b03e967a0188bd40309526550fc2e81ddd
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-08-30 23:37:02 +0530

    practice stack

---
+++ STL/3_stack/03_stack_Basics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    
+    stack<int> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+
+    cout << st.size() << endl;
+    return 0;
+}
