fedf81125a25f17edf5d776eee478c6e2f22c712
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-08-30 13:03:40 +0530

    practice pair

---
+++ STL/2_Pair/05_Pair_Operations.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<utility>
+using namespace std;
+
+int main() {
+    
+    pair<int,int> p1 = {10,20};
+    pair<int,int> p2 = {30,40};
+
+    p1.swap(p2);
+
+    cout << "p1 values :" << p1.first << " " << p1.second << endl;
+    cout << "p2 values :" << p2.first << " " << p2.second << endl;
+    return 0;
+}
