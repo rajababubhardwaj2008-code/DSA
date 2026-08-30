88dc4759cc979a57784b0fb36a812be45cf82640
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-08-30 13:35:54 +0530

    practice pair

---
+++ STL/2_Pair/08_Pair_Operations.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<utility>
+using namespace std;
+
+int main() {
+
+    pair<int,int> p ={10,20};
+
+    cout << "sum is = " << p.first + p.second << endl;
+    return 0;
+}
