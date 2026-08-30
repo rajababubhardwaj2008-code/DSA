0b408b3f272cdc04c1ec41da1d7a3dc1839b9e0c
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-08-30 11:49:52 +0530

    practice pair

---
+++ STL/2_Pair/02_Pair_Basics.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<utility>
+using namespace std;
+
+int main() {
+    pair<int,string> p = {101,"Raja Babu"};
+
+    cout << p.first << endl;
+    cout << p.second << endl;
+}
