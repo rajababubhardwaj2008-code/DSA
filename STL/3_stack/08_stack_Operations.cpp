5b169bfeb7e2372670e45adb2fd7997805453636
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-08-31 14:14:53 +0530

    practice stack

---
+++ STL/3_stack/08_stack_Operations.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    stack<int> st;
+    st.push(10);
+    st.push(25);
+    st.push(30);
+    st.push(15);
+    st.push(40);
+
+    int count = 0;
+    while(!st.empty()){
+        if( st.top() > 20){
+            count++; //forward the count
+        }
+        st.pop();// it removes the top element so that top element cannot be stay forever.
+    }
+    cout << "elements larger than 20 are :" << count << endl;
+    return 0;
+}
