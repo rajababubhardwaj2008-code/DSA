3007df26802404ada374e42c07c24f8a00623e8e
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-09-02 21:23:49 +0530

    Update applying deque

--- STL/5_Deque/06_Applying_deque.cpp
+++ STL/5_Deque/06_Applying_deque.cpp
@@ -1,28 +1,30 @@
-#include<iostream>
-#include<deque>
+#include <iostream>
+#include <deque>
 using namespace std;
 
 int main() {
-    deque<int> dq = {10,20,30,33,22};
+    deque<int> dq = {10, 20, 30, 33, 22};
 
     int target;
-    cout << "enter target" << endl;
-    cin>> target;
+    cout << "Enter target: " << endl;
+    cin >> target;
+    
     bool found = false;
 
-
-    for( int i = 0; i<dq.size(); i++){
-        if(dq[i] == target){
+  
+    for (int i = 0; i < dq.size(); i++) {
+        if (dq[i] == target) {
             found = true;
-            break;
+            break; 
         }
-        if(dq.empty()){
-            cout << "element not found" << endl;
-        }
-           
-        
-        
     }
-    cout << "element found" << target << endl;
+    
+   
+    if (found) {
+        cout << "Element found" << target << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
     return 0;
 }
