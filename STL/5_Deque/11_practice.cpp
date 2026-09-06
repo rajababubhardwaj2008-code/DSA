77f85f6168c63dc87bcaef434071198c5c3b536e
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-09-06 23:55:18 +0530

    practice deque

---
+++ STL/5_Deque/11_practice.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        result.reserve(s.size()); 
+        
+        int opened = 0;
+        deque<char> dq(s.begin(), s.end());
+        
+        while (!dq.empty()) {
+            char ch = dq.front();
+            dq.pop_front(); 
+            
+            if (ch == '(') {
+                if (opened > 0) {
+                    result.push_back(ch); 
+                }
+                opened++;
+            } else {
+                opened--;
+                if (opened > 0) {
+                    result.push_back(ch); 
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+// ADD THIS MAIN FUNCTION AT THE BOTTOM OF YOUR FILE
+int main() {
+    Solution solver;
+    string input = "(()())(())";
+    
+    string output = solver.removeOuterParentheses(input);
+    
+    cout << "Input:  " << input << endl;
+    cout << "Output: " << output << endl;
+    
+    return 0;
+}
