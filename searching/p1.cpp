10ed2bdf68b2fd5880b3f17630dadd501e54452b
Raja babu bhardwaj -7254 <rajababubhardwaj2008@gmail.com>
2026-09-11 20:51:16 +0530

    practice searching

--- searching/p1.cpp
+++ searching/p1.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 #include<vector>
-#include<array>
 using namespace std;
 
-int main() {
-    vector<int> arr = {5,3,8,1,9};
-    int target = 100;
-    for( int i = 0; i<arr.size(); i++){
-        if(arr[i] == target ){
-           cout << "index value is :" << i << endl;
-           return 0;
+int linearSearch(vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target) {
+            return i;
         }
     }
-    cout <<"index not find"<<endl;
     return -1;
+}
+
+int main() {
+    vector<int> arr = {5, 3, 8, 1, 9};
+    int target = 8;
+    
+    int index = linearSearch(arr, target);
+    
+    cout << "index: " << index << endl;
+    return 0;
 }
